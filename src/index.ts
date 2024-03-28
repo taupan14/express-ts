ac54be8f5a92c10090dac82ba6dd49a5d6596a54
Taupan T <taupan.taufik@gmail.com>
2024-03-29 00:47:26 +0700

    initialize project

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+// console.log('Hello World')
+
+import express, { type Request, type Application, type Response, type NextFunction } from 'express'
+import 'dotenv/config'
+
+const app: Application = express()
+const port: number = process.env.PORT != null ? parseInt(process.env.PORT) : 3000
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+    res.send('Hello World!');
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on http://localhost:${port}`)
+})
